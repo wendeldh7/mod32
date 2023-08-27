878081e4d2d1c33018155ffe3b1fff6de704e53c
Wendel Gomes <wendelgomesdh7@gmail.com>
2023-08-27 11:52:45 -0400

    primeiro commit

---
+++ ExemploVendasMod30/src/main/java/br/com/gomes/services/IClienteService.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package br.com.gomes.services;
+
+import br.com.gomes.domain.Cliente;
+import br.com.gomes.exceptions.DAOException;
+import br.com.gomes.exceptions.TipoChaveNaoEncontradaException;
+import br.com.gomes.services.generic.IGenericService;
+
+/**
+ * @author wendel.gomes
+ *
+ */
+public interface IClienteService extends IGenericService<Cliente, Long> {
+
+//	Boolean cadastrar(Cliente cliente) throws TipoChaveNaoEncontradaException;
+//
+	Cliente buscarPorCPF(Long cpf) throws DAOException;
+//
+//	void excluir(Long cpf);
+//
+//	void alterar(Cliente cliente) throws TipoChaveNaoEncontradaException;
+
+}
